d48ed6d8ae91a23954fb30d9e7e8b1a8f3c1760e
Saberon, John Paul (JeyP) <johnpaulsaberon1428@gmail.com>
2024-10-29 22:18:28 +0800

    Add files via upload

---
+++ frmLoginAccount/frmLoginAccount/Form1.Designer.cs
@@ -0,0 +1,108 @@
+﻿namespace frmLoginAccount
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.Username_label = new System.Windows.Forms.Label();
+            this.Password_label = new System.Windows.Forms.Label();
+            this.Usernametxt = new System.Windows.Forms.TextBox();
+            this.Passwordtxt = new System.Windows.Forms.TextBox();
+            this.Login_button = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // Username_label
+            // 
+            this.Username_label.AutoSize = true;
+            this.Username_label.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Username_label.Location = new System.Drawing.Point(122, 51);
+            this.Username_label.Name = "Username_label";
+            this.Username_label.Size = new System.Drawing.Size(81, 16);
+            this.Username_label.TabIndex = 0;
+            this.Username_label.Text = "UserName";
+            // 
+            // Password_label
+            // 
+            this.Password_label.AutoSize = true;
+            this.Password_label.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Password_label.Location = new System.Drawing.Point(122, 132);
+            this.Password_label.Name = "Password_label";
+            this.Password_label.Size = new System.Drawing.Size(75, 16);
+            this.Password_label.TabIndex = 1;
+            this.Password_label.Text = "Password";
+            // 
+            // Usernametxt
+            // 
+            this.Usernametxt.Location = new System.Drawing.Point(125, 70);
+            this.Usernametxt.Name = "Usernametxt";
+            this.Usernametxt.Size = new System.Drawing.Size(136, 20);
+            this.Usernametxt.TabIndex = 2;
+            // 
+            // Passwordtxt
+            // 
+            this.Passwordtxt.Location = new System.Drawing.Point(125, 151);
+            this.Passwordtxt.Name = "Passwordtxt";
+            this.Passwordtxt.Size = new System.Drawing.Size(136, 20);
+            this.Passwordtxt.TabIndex = 3;
+            // 
+            // Login_button
+            // 
+            this.Login_button.Location = new System.Drawing.Point(116, 191);
+            this.Login_button.Name = "Login_button";
+            this.Login_button.Size = new System.Drawing.Size(161, 39);
+            this.Login_button.TabIndex = 4;
+            this.Login_button.Text = "Submit";
+            this.Login_button.UseVisualStyleBackColor = true;
+            this.Login_button.Click += new System.EventHandler(this.btnLogin_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(422, 284);
+            this.Controls.Add(this.Login_button);
+            this.Controls.Add(this.Passwordtxt);
+            this.Controls.Add(this.Usernametxt);
+            this.Controls.Add(this.Password_label);
+            this.Controls.Add(this.Username_label);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label Username_label;
+        private System.Windows.Forms.Label Password_label;
+        private System.Windows.Forms.TextBox Usernametxt;
+        private System.Windows.Forms.TextBox Passwordtxt;
+        private System.Windows.Forms.Button Login_button;
+    }
+}
+
